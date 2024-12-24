083cbb884fc63b22c525577fdf97e85b6b3cec27
Anatoly <acryptokrestenko@gmail.com>
2024-12-24 21:39:21 +0700

    docker update

--- bot/texts.py
+++ bot/texts.py
@@ -1,7 +1,10 @@
 START_TEXT = """
 Hello! I'm AI Crytpro Pulse bot that can help you with your daily crypto news.
+
 I will filter the news for you and provide you with the most important information.
-Please let me know in what you are interested in.
+Please let me know in what you are interested in. Example:
+
+I’m interested in DeFi and AI infrstructure. My moprtfolio: BTC, ETH, HYPE. Send me news in human style like you are my bro that should tell me about juicy insides. Be precise and compact in your answers
 """
 
 REGULAR_UPDATE_PROMPT = """
